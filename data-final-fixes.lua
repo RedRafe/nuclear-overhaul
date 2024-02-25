fc52a70ebae8923574167a3127da8b61d7828414
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-02-25 17:14:21 +0100

    release v1.1.3

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,1 +1,9 @@
 nuclear.debug.log("-- --final-fixes")
+
+for _, tech in pairs(data.raw.technology) do
+  for i, p in pairs(tech.prerequisites or {}) do
+    if p == "nuclear-fuel-reprocessing" then
+      tech.prerequisites[i] = "nuclear-fuel-reprocessing-1"
+    end
+  end
+end
