a7688dc9f74953c93bae744451a3a39979011a1a
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-06-15 22:53:17 -0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -12,3 +12,66 @@ local handler = require '__core__.lualib.event_handler'
 handler.add_libraries{
   require 'scripts.reactor-failure',
 }
+
+-- Add support for K2 radioactivity
+script.on_init(function()
+  if remote.interfaces["kr-radioactivity"] 
+    and remote.interfaces["kr-radioactivity"]["add_entity"]
+    and remote.interfaces["kr-radioactivity"]["add_item"]
+  then
+    remote.call("kr-radioactivity", "add_entity", "thorium-ore" )
+    remote.call("kr-radioactivity", "add_item", "neptunium-237")
+    remote.call("kr-radioactivity", "add_item", "neptunium-237")
+    remote.call("kr-radioactivity", "add_item", "plutonium-238")
+    remote.call("kr-radioactivity", "add_item", "plutonium-239")
+    remote.call("kr-radioactivity", "add_item", "plutonium-fuel")
+    remote.call("kr-radioactivity", "add_item", "plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "protactinium-233")
+    remote.call("kr-radioactivity", "add_item", "protactinium-234")
+    remote.call("kr-radioactivity", "add_item", "thorium-232")
+    remote.call("kr-radioactivity", "add_item", "thorium-233")
+    remote.call("kr-radioactivity", "add_item", "thorium-fuel")
+    remote.call("kr-radioactivity", "add_item", "thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "thorium-ore")
+    remote.call("kr-radioactivity", "add_item", "uranium-233")
+    remote.call("kr-radioactivity", "add_item", "uranium-234")
+    remote.call("kr-radioactivity", "add_item", "uranium-236")
+    remote.call("kr-radioactivity", "add_item", "uranium-237")
+    remote.call("kr-radioactivity", "add_item", "uranium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-uranium-mox")
+    remote.call("kr-radioactivity", "add_item", "yellowcake")
+  end
+end)
+
+script.on_load(function()
+  if remote.interfaces["kr-radioactivity"] 
+    and remote.interfaces["kr-radioactivity"]["add_entity"]
+    and remote.interfaces["kr-radioactivity"]["add_item"]
+  then
+    remote.call("kr-radioactivity", "add_entity", "thorium-ore" )
+    remote.call("kr-radioactivity", "add_item", "neptunium-237")
+    remote.call("kr-radioactivity", "add_item", "neptunium-237")
+    remote.call("kr-radioactivity", "add_item", "plutonium-238")
+    remote.call("kr-radioactivity", "add_item", "plutonium-239")
+    remote.call("kr-radioactivity", "add_item", "plutonium-fuel")
+    remote.call("kr-radioactivity", "add_item", "plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "protactinium-233")
+    remote.call("kr-radioactivity", "add_item", "protactinium-234")
+    remote.call("kr-radioactivity", "add_item", "thorium-232")
+    remote.call("kr-radioactivity", "add_item", "thorium-233")
+    remote.call("kr-radioactivity", "add_item", "thorium-fuel")
+    remote.call("kr-radioactivity", "add_item", "thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "thorium-ore")
+    remote.call("kr-radioactivity", "add_item", "uranium-233")
+    remote.call("kr-radioactivity", "add_item", "uranium-234")
+    remote.call("kr-radioactivity", "add_item", "uranium-236")
+    remote.call("kr-radioactivity", "add_item", "uranium-237")
+    remote.call("kr-radioactivity", "add_item", "uranium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-uranium-mox")
+    remote.call("kr-radioactivity", "add_item", "yellowcake")
+  end
+end)
